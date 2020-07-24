b4bc7fd7e11b22d0e08f9849202198c3235ee025
Anton Webb <anton25360@gmail.com>
2020-07-24 11:16:50 +0100

    added item-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddItemFormComponent } from './add-item-form/add-item-form.component';
+import { ItemListComponent } from './item-list/item-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddItemFormComponent
+    AddItemFormComponent,
+    ItemListComponent
   ],
   imports: [
     BrowserModule,
