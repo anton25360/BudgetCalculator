cd2db9e37d868a52eb88bb7026e90c0836514527
Anton Webb <anton25360@gmail.com>
2020-07-24 10:56:44 +0100

    added new form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddItemFormComponent } from './add-item-form/add-item-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddItemFormComponent
   ],
   imports: [
     BrowserModule,
