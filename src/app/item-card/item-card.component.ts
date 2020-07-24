32b8fda7fe8e4b76d4fa0a532873e59f6f44b216
Anton Webb <anton25360@gmail.com>
2020-07-24 12:16:45 +0100

    added logic for changing class based on income

--- src/app/item-card/item-card.component.ts
+++ src/app/item-card/item-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-item-card',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item-card.component.scss']
 })
 export class ItemCardComponent implements OnInit {
+
+  @Input() isIncome:boolean = false
 
   constructor() { }
 
