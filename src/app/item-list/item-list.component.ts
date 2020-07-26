90df31f242a9bd152ffdb21fbaab14dfff6e9139
Anton Webb <anton25360@gmail.com>
2020-07-26 11:30:27 +0100

    item updates

--- src/app/item-list/item-list.component.ts
+++ src/app/item-list/item-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { ItemCard } from 'src/shared/models/item-card.model';
 import {MatDialogModule, MatDialog} from '@angular/material/dialog';
+import { EditItemModalComponent } from '../edit-item-modal/edit-item-modal.component';
 
 @Component({
   selector: 'app-item-list',
@@ -23,7 +24,17 @@ export class ItemListComponent implements OnInit {
 
   onCardClicked(item:ItemCard){
     //show edit modal
-    const dialogRef = this.dialog.open
-  }
+    const dialogRef = this.dialog.open(EditItemModalComponent, {
+      width: '580px',
+      data: item
+    })
 
+    //check if result has a value
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        //replace item with updaed values from the form
+        this.itemsArray[this.itemsArray.indexOf(item)] = result //result is the updated item
+      }
+    })
+  }
 }
